5b08aff7d8ba9d4716691efcfdda3a22d7de37cf
Armin Ulrich <armininterstellar@gmail.com>
2023-09-25 16:13:18 -0400

    Initilized map component and imported on index page

---
+++ application/app/components/Map.jsx
@@ -0,0 +1,9 @@
+
+
+export function Map(props) {
+    return(
+        <div>
+            <h3>This is the map component</h3>
+        </div>
+    )
+}
--- application/app/page.js
+++ application/app/page.js
@@ -1,9 +1,10 @@
-
+import { Map } from './components/Map';
 
 export default function Home() {
   return (
     <main>
       <h1>Qpath Optimizer</h1>
+      <Map/>
     </main>
   )
 }
