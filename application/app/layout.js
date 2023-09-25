9493171c5678dfb7c9128654edf61f1c0685de53
Armin Ulrich <armininterstellar@gmail.com>
2023-09-25 16:22:09 -0400

    For now: Added chakraUI providers to route to enable chakraui component usage

--- application/app/layout.js
+++ application/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
     </html>
   )
 }
--- application/app/page.js
+++ application/app/page.js
@@ -1,9 +1,13 @@
+'use client'
+
+import { CircularProgress } from '@chakra-ui/progress';
 import { Map } from './components/Map';
 
 export default function Home() {
   return (
     <main>
       <h1>Qpath Optimizer</h1>
+      <CircularProgress isIndeterminate color='green.300' />
       <Map/>
     </main>
   )
