6929c12f22e67aa3e779ee952584f26e3aebef7d
lapisliu <lxteng2016@126.com>
2023-09-25 23:53:44 -0400

    "feat:map showing start mid and end points"

--- application/app/components/Map.jsx
+++ application/app/components/Map.jsx
@@ -1,9 +1,67 @@
+import React from "react";
+import { Avatar, AvatarBadge } from "@chakra-ui/react";
+import GoogleMapReact from "google-map-react";
 
+require("dotenv").config();
 
-export function Map(props) {
-    return(
-        <div>
-            <h3>This is the map component</h3>
-        </div>
-    )
+/**
+ * Populates the provide state variables with the startpoint, midpoints, and endpoint
+ * @param {object:{lat,lng}} startpoint
+ * @param {array:[{lat,lng},...,{lat,lng}]} midpoints
+ * @param {object:{lat,lng}} endpoint
+ * @param {function} setStartpoint
+ * @param {function} setMidpoints
+ * @param {function} setEndpoint
+ * @returns
+ */
+export function Map({
+  startpoint,
+  endpoint,
+  midpoints,
+  setStartpoint,
+  setEndpoint,
+  setMidpoints,
+}) {
+  startpoint = { lat: 40.7731, lng: -73.991321 }; // should get from user input
+  midpoints = [
+    { lat: 40.7631, lng: -73.991321 },
+    { lat: 40.7531, lng: -73.991321 },
+    { lat: 40.7431, lng: -73.991321 },
+  ]; // should get from api call to backend
+  endpoint = { lat: 40.7331, lng: -73.971321 }; // should get from user input
+
+  const defaultGeoLoc = {
+    center: {
+      lat: 40.7431,
+      lng: -73.991321,
+    },
+    zoom: 13,
+  };
+  const handleApiLoaded = (map, maps) => {
+    // use map and maps objects
+  };
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
+        defaultCenter={defaultGeoLoc.center}
+        defaultZoom={defaultGeoLoc.zoom}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+      >
+        <Avatar lat={startpoint.lat} lng={startpoint.lng}>
+          <AvatarBadge boxSize="24px" bg="green.500" />
+        </Avatar>
+        {midpoints.map((midpoint, index) => (
+          <Avatar key={index} lat={midpoint.lat} lng={midpoint.lng}>
+            <AvatarBadge boxSize="16px" bg="#696969" />
+          </Avatar>
+        ))}
+        <Avatar lat={endpoint.lat} lng={endpoint.lng}>
+          <AvatarBadge boxSize="24px" bg="#7C5CDA" />
+        </Avatar>
+      </GoogleMapReact>
+    </div>
+  );
 }
