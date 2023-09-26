ea2fb4b9072d6c90cd3848c974d4a70a2f8f0fbc
lapisliu <lxteng2016@126.com>
2023-09-26 00:29:30 -0400

    fix:passed states down from the root page

--- application/app/page.js
+++ application/app/page.js
@@ -1,14 +1,20 @@
-'use client'
-
-import { CircularProgress } from '@chakra-ui/progress';
-import { Map } from './components/Map';
+"use client";
+import React, { useState } from "react";
+import { CircularProgress } from "@chakra-ui/progress";
+import { Map } from "./components/Map";
 
 export default function Home() {
+  const [startpoint, setStartpoint] = useState({
+    lat: 40.7731,
+    lng: -73.991321,
+  });
+  const [midpoints, setMidpoints] = useState([]);
+  const [endpoint, setEndpoint] = useState({ lat: 40.7331, lng: -73.971321 });
   return (
     <main>
       <h1>Qpath Optimizer</h1>
-      <CircularProgress isIndeterminate color='green.300' />
-      <Map/>
+      <CircularProgress isIndeterminate color="green.300" />
+      <Map startpoint={startpoint} midpoints={midpoints} endpoint={endpoint} />
     </main>
-  )
+  );
 }
