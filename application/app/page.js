c7614c629ce723076eb0e5983181d2ccf6ba395c
lapisliu <lxteng2016@126.com>
2023-09-27 17:38:15 -0400

    chore: default stop location for testing

--- application/app/page.js
+++ application/app/page.js
@@ -1,17 +1,21 @@
 "use client";
 import React, { useState } from "react";
-import { CircularProgress } from "@chakra-ui/progress";
 import { Map } from "./components/Map";
 
 export default function Home() {
   const [startpoint, setStartpoint] = useState();
-  const [midpoints, setMidpoints] = useState([]);
-  const [endpoint, setEndpoint] = useState();
+  const [midpoints, setMidpoints] = useState([
+    { lat: 40.7431, lng: -73.971321 },
+    { lat: 40.7531, lng: -73.961321 },
+  ]); //default value for testing. Remove when done
+  const [endpoint, setEndpoint] = useState({
+    lat: 40.7431,
+    lng: -73.991321,
+  }); //default value for testing. Remove when done
   const [choosingStartpoint, setChoosingStartpoint] = useState(true);
   return (
     <main>
       <h1>Qpath Optimizer</h1>
-      <CircularProgress isIndeterminate color="green.300" />
       <Map
         startpoint={startpoint}
         midpoints={midpoints}
